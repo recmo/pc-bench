739a1233cfacc5b1e9f790192eaf4dd06962702c
Marcin Kostrzewa <marckostrzewa@gmail.com>
2023-07-03 17:42:10 +0200

    add batching to gnark

--- gnark/pc-gnark.go
+++ gnark/pc-gnark.go
@@ -4,25 +4,46 @@ import (
 	"fmt"
 	"math"
 	"math/big"
-	"time"
 	"reflect"
-	
-	"github.com/consensys/gnark-crypto/ecc/bn254/fr/kzg"
+	"time"
+
 	"github.com/consensys/gnark-crypto/ecc/bn254"
 	"github.com/consensys/gnark-crypto/ecc/bn254/fr"
+	"github.com/consensys/gnark-crypto/ecc/bn254/fr/kzg"
 )
 
+func parallelize[I any, O any](op func(I) (O, error), inputs []I) []O {
+	outChans := make([]chan O, len(inputs))
+	for i, input := range inputs {
+		outChans[i] = make(chan O)
+		go func(input I, outChan chan O) {
+			res, err := op(input)
+			if err != nil {
+				panic(err)
+			}
+			outChan <- res
+		}(input, outChans[i])
+	}
+	outputs := make([]O, len(inputs))
+	for i, outChan := range outChans {
+		outputs[i] = <-outChan
+		close(outChan)
+	}
+	return outputs
+}
+
 func main() {
-	const max_exp = 30;
-	const max_size = 1 << max_exp;
-	const divisions = 8;
+	const maxExp = 28
+	const maxSize = 1 << maxExp
+	const batchSize = 16
+	const divisions = 8
 
 	// Generate SRS
-    fmt.Printf("Allocating SRS (%f GB).\n",  float64(reflect.TypeOf((*bn254.G1Affine)(nil)).Elem().Size()) * float64(max_size) / 1.0e9 );
+	fmt.Printf("Allocating SRS (%f GB).\n", float64(reflect.TypeOf((*bn254.G1Affine)(nil)).Elem().Size())*float64(maxSize)/1.0e9)
 	// srs := kzg.SRS{
 	// 	G1: make([]bn254.G1Affine, max_size),
 	// };
-    // fmt.Printf("Generating random points.\n")
+	// fmt.Printf("Generating random points.\n")
 	// start := time.Now()
 
 	// _, _, gen1Aff, gen2Aff := bn254.Generators()
@@ -34,66 +55,64 @@ func main() {
 	// }
 
 	start := time.Now()
-	srs, err := kzg.NewSRS(max_size, new(big.Int).SetInt64(1337))
+	srs, err := kzg.NewSRS(maxSize, new(big.Int).SetInt64(1337))
 	if err != nil {
 		panic(err)
 	}
-    fmt.Printf("Generating SRS done in %s.\n", time.Now().Sub(start))
+	fmt.Printf("Generating SRS done in %s.\n", time.Now().Sub(start))
 
 	// Create a polynomial
-    fmt.Println("Generating scalars.")
+	fmt.Println("Generating scalars.")
 	start = time.Now()
-	f := make([]fr.Element, max_size)
-	for i := 0; i < max_size; i++ {
-		f[i].SetRandom()
+	f := make([][]fr.Element, batchSize)
+	for i := 0; i < batchSize; i++ {
+		f[i] = make([]fr.Element, maxSize)
+		for j := 0; j < maxSize; j++ {
+			f[i][j].SetRandom()
+		}
 	}
-    fmt.Printf("Generating scalars done in %s.\n", time.Now().Sub(start))
+	fmt.Printf("Generating scalars done in %s.\n", time.Now().Sub(start))
 
 	fmt.Println("size,duration,throughput")
 
 	// Commit polynomial
-	for i := 10; i <= max_exp; i++ {
-		var size = 1 << i;
+	for i := 10; i <= maxExp; i++ {
+		var size = 1 << i
 
-		var duration = 0.0;
-		var count = 0;
+		var duration = 0.0
+		var count = 0
 
 		for duration < 5.0 {
 			start := time.Now()
-			_, err := kzg.Commit(f[0:size], srs)
-			if err != nil {
-				panic(err)
-			}
-			duration += time.Now().Sub(start).Seconds();
-			count += 1;
+			op := func(inp []fr.Element) (kzg.Digest, error) { return kzg.Commit(inp[0:size], srs) }
+			parallelize(op, f)
+			duration += time.Now().Sub(start).Seconds()
+			count += 1
 		}
-		duration /= float64(count);
+		duration /= float64(count)
 
-		throughput := float64(size) / duration;
-		fmt.Printf("%d,%f,%f\n", size, duration, throughput);
+		throughput := float64(size*batchSize) / duration
+		fmt.Printf("%d,%f,%f\n", size, duration, throughput)
 
-
-		var base_size = size;
-		if i < max_exp  {
+		var baseSize = size
+		if i < maxExp {
 			for j := 1; j < divisions; j++ {
-				var size = int( float64(base_size) * math.Pow(2.0, float64(j) / float64(divisions)) );
+				var size = int(float64(baseSize) * math.Pow(2.0, float64(j)/float64(divisions)))
 
-				var duration = 0.0;
-				var count = 0;
+				var duration = 0.0
+				var count = 0
 
 				for duration < 5.0 {
 					start := time.Now()
-					_, err := kzg.Commit(f[0:size], srs)
-					if err != nil {
-						panic(err)
-					}
-					duration += time.Now().Sub(start).Seconds();
-					count += 1;
+					op := func(inp []fr.Element) (kzg.Digest, error) { return kzg.Commit(inp[0:size], srs) }
+					parallelize(op, f)
+					duration += time.Now().Sub(start).Seconds()
+					count += 1
 				}
-				duration /= float64(count);
+				duration /= float64(count)
 
-				throughput := float64(size) / duration;
-				fmt.Printf("%d,%f,%f\n", size, duration, throughput);
+				throughput := float64(size) / duration
+				fmt.Printf("%d,%f,%f\n", size, duration, throughput)
 
 			}
 		}
